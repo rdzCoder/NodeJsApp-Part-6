0b4555fefa704c7edd494af39405a946e92fde07
rdz <gilles452@gmail.com>
2020-08-01 17:40:22 +0200

    Adding changes part 6

--- routes/web/post.js
+++ routes/web/post.js
@@ -36,7 +36,39 @@ router.get("/", function(req, res){
 
  });
 
+ //: means a route parameter it could be anything and it's often an ID
+ // localhost:3000/posts/12345 --> fetch the post with id 12345
+router.get("/:postId", function(req,res){
+    Post.findById(req.params.postId).exec(function(err, post){
+        res.render("post/detailpost",{post:post});
+    });
+});
 
+router.get("/edit/:postId", function(req,res){
+    Post.findById(req.params.postId).exec(function(err, post){
+        res.render("post/editpost",{post:post});
+    });
+});
+
+router.post("/update", async function(req, res){
+     const post = await Post.findById(req.body.postid);
+     
+     post.title = req.body.title;
+     post.content = req.body.content;
+
+     // post.save()
+
+     try {
+         let savePost = await post.save();
+         console.log("savepost", savePost);
+         res.redirect("/posts/" + req.body.postid);
+
+     } catch (err) {
+         console.log("error happened");
+         res.status(500).send(err);
+     }
+
+});
 
 
 module.exports = router;
